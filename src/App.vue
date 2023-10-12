8ef89660948a33bd01e44267dd952c159c9cd5fe
Kristofer <kristofer.valk@tptlive.ee>
2023-10-12 11:32:39 +0300

    tund

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
-
+import { ref } from 'vue';
+const message = ref('Hello vue');
+const items = ref(['leib', 'sai', 'piim', 'viin']);
+function add(){
+    this.items.push(this.message);
+}
 </script>
 
 <template>
-
+    <button @click="message=items.push(message)">Click me</button>
+    <input type="text" v-model="message">
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+    <ul>
+        <li v-for="item in items">{{ item }}</li>
+    </ul>
 </template>
