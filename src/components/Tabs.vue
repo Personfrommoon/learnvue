6a48dc8567d9723259c3349edd197afca50e861e
Kristofer <kristofer.valk@tptlive.ee>
2023-11-23 11:15:19 +0200

    niger

---
+++ src/components/Tabs.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="tabs is-centered">
+        <ul>
+            <li class="is-active"><a href="/">Home</a></li>
+            <li><a href="/#/todo"></a></li>
+        </ul>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './style.scss'
+import App from './App.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+import Home from './pages/Home.vue';
+import ToDo from './pages/ToDo.vue';
+const routes = [
+    { path: '/', component: Home }
+    { path: '/todo', component: ToDo }
+]
+
+const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+
+  const app = createApp(App);
+
+  app.use(router);
+
+  app.mount(#app);
 
 createApp(App).mount('#app');
