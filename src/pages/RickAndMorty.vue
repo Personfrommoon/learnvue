1fc54a7526ef8e39f1e3957a27f1e1275632032d
Personfrommoon <kristofer.v06@gmail.com>
2024-01-24 11:15:23 +0200

    next and prev

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -6,17 +6,41 @@ import CharacterCard from '../components/CharacterCard.vue'
 let info = ref({});
 let chars = ref([]);
 
-let response = await axios.get('https://rickandmortyapi.com/api/character');
-console.log(response.data);
-info.value = response.data.info;
-chars.value = response.data.results;
+getCharacters('https://rickandmortyapi.com/api/character')
+
+async function getCharacters(url) {
+    let response = await axios.get(url);
+    console.log(response.data);
+    info.value = response.data.info;
+    chars.value = response.data.results;
+}
+async function next() {
+    getCharacters(info.value.next)
+}
+
+async function prev() {
+    getCharacters(info.value.prev)
+}
+
 </script>
 
 <template>
     <div class="container">
+        <div class="field has-addons">
+            <p class="control">
+                <button :disabled="info.prev === null" class="button " @click="prev()">
+                    <span>Previous</span>
+                </button>
+            </p>
+            <p class="control">
+                <button :disabled="!info.next" class="button" @click="next()">
+                    <span>Next</span>
+                </button>
+            </p>
+        </div>
         <div class="columns is-multiline">
             <div v-for="char in chars" :key="char.id" class="column is-one-quarter">
-                    <CharacterCard :character="char"></CharacterCard>
+                <CharacterCard :character="char"></CharacterCard>
             </div>
         </div>
     </div>
