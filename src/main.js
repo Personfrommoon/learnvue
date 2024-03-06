5e98779c8490c6d862883d054ee245d3cda182e1
Personfrommoon <kristofer.v06@gmail.com>
2024-03-06 10:16:31 +0200

    ....

--- src/main.js
+++ src/main.js
@@ -1,26 +1,25 @@
 import { createApp } from 'vue';
 import './style.scss';
 import App from './App.vue';
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from './pages/Home.vue';
 import ToDo from './pages/ToDo.vue';
 import Modals from './pages/Modals.vue';
-import Messages from './pages/Messages.vue';
 import ChuckNorris from './pages/ChuckNorris.vue';
 import RickAndMorty from './pages/RickAndMorty.vue';
 import CanvasExample from './pages/CanvasExample.vue';
 import MapExample from './pages/MapExample.vue';
-
+import Websocket from './pages/Websocket.vue';
 const routes = [
     { path: '/', component: Home, name: 'Home' },
     { path: '/todo', component: ToDo, name: 'ToDo' },
     { path: '/modals', component: Modals, name: 'Modals' },
-    { path: '/messages', component: Messages, name: 'Message' },
-    { path: '/chucknorris', component: ChuckNorris, name: 'Chuck Norris'},
+    { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris' },
     { path: '/rickandmorty', component: RickAndMorty, name: 'Rick And Morty' },
     { path: '/canvas', component: CanvasExample, name: 'Canvas' },
-    { path: '/map', component: MapExample, name: 'Map' },
+    { path: '/maps', component: MapExample, name: 'Map Example' },
+    { path: '/ws', component: Websocket, name: 'Websocket' },
 ];
 
 const router = createRouter({
@@ -34,3 +33,7 @@ const app = createApp(App);
 app.use(router);
 
 app.mount('#app');
+
+if(app == null) {
+    window.a = null;
+}
