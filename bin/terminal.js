12100985e5995b161ec4c731dc86083c9e847ea9
Personfrommoon <kristofer.v06@gmail.com>
2024-03-13 10:59:31 +0200

    web scraper

---
+++ bin/terminal.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import fs from 'node:fs';
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+if(!fs.existsSync('cache')){
+    fs.mkdirSync('cache');
+}
+
+for(let i = 2905; i > 2895; i--){
+
+    let cacheName = `cache/${i}.html`;
+    let data;
+
+    if(!fs.existsSync(`cache/${cacheName}`)){
+        await sleep(1000);
+        let res = await axios.get(`https://xkcd.com/${i}/`);
+        data = res.data;
+        fs.writeFileSync(cacheName, data);
+        console.log('LIVE REQUEST!!!!!')
+    } 
+    else {
+        data = fs.readFileSync(cacheName);
+    }
+    //console.log(res.data);
+    const $ = cheerio.load(data);
+    let img = $('div#comic>img');
+    console.log(img.attr('src'));
+    console.log(img.attr('title'));
+    console.log(img.attr('alt'));
+    
+}
