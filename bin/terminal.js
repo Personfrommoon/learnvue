e498b7cc63b39b89e405aa1d38f942af5206e87f
Personfrommoon <kristofer.v06@gmail.com>
2024-03-13 11:03:38 +0200

    little change

--- bin/terminal.js
+++ bin/terminal.js
@@ -9,25 +9,23 @@ if(!fs.existsSync('cache')){
 }
 
 for(let i = 2905; i > 2895; i--){
-
     let cacheName = `cache/${i}.html`;
     let data;
 
-    if(!fs.existsSync(`cache/${cacheName}`)){
+    if(!fs.existsSync(cacheName)){
         await sleep(1000);
         let res = await axios.get(`https://xkcd.com/${i}/`);
         data = res.data;
         fs.writeFileSync(cacheName, data);
-        console.log('LIVE REQUEST!!!!!')
-    } 
-    else {
+        console.log('LIVE REQUEST!!!!');
+    } else {
         data = fs.readFileSync(cacheName);
     }
+
     //console.log(res.data);
     const $ = cheerio.load(data);
     let img = $('div#comic>img');
     console.log(img.attr('src'));
     console.log(img.attr('title'));
     console.log(img.attr('alt'));
-    
 }
